7c5571cda6f5fa3f5ffdd37b7237286637c7da08
Gabriel Escobar <gabrielgbg@gmail.com>
2022-02-10 11:14:08 -0300

    Created a method to create and return a prepared statement, another method to execute a query and return a result set, add empty lines in the EOF and another requested fixes

--- src/main/java/DataBaseInteracting.java
+++ src/main/java/DataBaseInteracting.java
@@ -19,52 +19,59 @@ import org.apache.arrow.vector.VectorSchemaRoot;
 import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.types.pojo.Schema;
 
-
 public class DataBaseInteracting {
-  static final RootAllocator allocator = new RootAllocator(Long.MAX_VALUE);
+  final RootAllocator allocator = new RootAllocator(Long.MAX_VALUE);
   private Connection connection = MakeConnection.getConnection();
 
   public DataBaseInteracting() throws SQLException {
   }
 
-  public List<Person> searchForAll() throws SQLException {
-    List<Person> listUser = new ArrayList<>();
+  public PreparedStatement createPrepareStatement() throws SQLException {
     String sql = "SELECT * FROM personalData";
-    try (PreparedStatement preparedStatement = connection.prepareStatement(sql);
-         ResultSet resultSet = preparedStatement.executeQuery();) {
-      while (resultSet.next()) {
-        Person person = new Person();
-        person.setId(resultSet.getInt("id"));
-        person.setName(resultSet.getString("name"));
-        person.setNumberrange(resultSet.getInt("numberrange"));
-        person.setCurrency(resultSet.getFloat("currency"));
-        listUser.add(person);
-      }
+    return connection.prepareStatement(sql);
+  }
+
+  public ResultSet createResultSet(PreparedStatement preparedStatement) throws SQLException {
+    return preparedStatement.executeQuery();
+  }
+
+  public List<Person> searchForAll(ResultSet resultSet) throws SQLException {
+    List<Person> listUser = new ArrayList<>();
+    while (resultSet.next()) {
+      Person person = new Person();
+      person.setId(resultSet.getInt("id"));
+      person.setName(resultSet.getString("name"));
+      person.setNumberrange(resultSet.getInt("numberrange"));
+      person.setCurrency(resultSet.getFloat("currency"));
+      listUser.add(person);
     }
     return listUser;
   }
 
   public VectorSchemaRoot dataBaseToVectorSchemaRoot(List<Person> list) {
     int rowCount = 0;
-    Schema schema = new Schema(Arrays.asList(
-        Field.nullable("id", INT.getType()),
-        Field.nullable("name", VARCHAR.getType()),
-        Field.nullable("numberrange", INT.getType()),
-        Field.nullable("currency", FLOAT4.getType()))
-    );
-    VectorSchemaRoot vectorSchemaRoot = VectorSchemaRoot.create(schema, allocator);
-    IntVector id = (IntVector) vectorSchemaRoot.getVector("id");
-    VarCharVector name = (VarCharVector) vectorSchemaRoot.getVector("name");
-    IntVector numberrange = (IntVector) vectorSchemaRoot.getVector("numberrange");
-    Float4Vector currency = (Float4Vector) vectorSchemaRoot.getVector("currency");
-    for (Person p : list) {
-      id.setSafe(rowCount, p.getId());
-      name.setSafe(rowCount, p.getName().getBytes(StandardCharsets.UTF_8));
-      numberrange.setSafe(rowCount, p.getNumberrange());
-      currency.setSafe(rowCount, p.getCurrency());
-      ++rowCount;
+    if(list.isEmpty()) {
+      throw new RuntimeException("Empty list");
     }
-    vectorSchemaRoot.setRowCount(rowCount);
-    return vectorSchemaRoot;
+      Schema schema = new Schema(Arrays.asList(
+          Field.nullable("id", INT.getType()),
+          Field.nullable("name", VARCHAR.getType()),
+          Field.nullable("numberrange", INT.getType()),
+          Field.nullable("currency", FLOAT4.getType()))
+      );
+      VectorSchemaRoot vectorSchemaRoot = VectorSchemaRoot.create(schema, allocator);
+      IntVector id = (IntVector) vectorSchemaRoot.getVector("id");
+      VarCharVector name = (VarCharVector) vectorSchemaRoot.getVector("name");
+      IntVector numberrange = (IntVector) vectorSchemaRoot.getVector("numberrange");
+      Float4Vector currency = (Float4Vector) vectorSchemaRoot.getVector("currency");
+      for (Person p : list) {
+        id.setSafe(rowCount, p.getId());
+        name.setSafe(rowCount, p.getName().getBytes(StandardCharsets.UTF_8));
+        numberrange.setSafe(rowCount, p.getNumberrange());
+        currency.setSafe(rowCount, p.getCurrency());
+        ++rowCount;
+      }
+      vectorSchemaRoot.setRowCount(rowCount);
+      return vectorSchemaRoot;
+    }
   }
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -6,8 +8,12 @@ import org.apache.arrow.vector.VectorSchemaRoot;
 public class Main {
   public static void main(String[] args) throws SQLException {
     DataBaseInteracting dataBaseInteracting = new DataBaseInteracting();
-    List<Person> list = dataBaseInteracting.searchForAll();
-    VectorSchemaRoot vectorSchemaRoot = dataBaseInteracting.dataBaseToVectorSchemaRoot(list);
-    System.out.println(vectorSchemaRoot.getFieldVectors());
+    try(PreparedStatement preparedStatement = dataBaseInteracting.createPrepareStatement();
+        ResultSet aResultSet = dataBaseInteracting.createResultSet(preparedStatement);) {
+      List<Person> list = dataBaseInteracting.searchForAll(aResultSet);
+      VectorSchemaRoot vectorSchemaRoot =
+          dataBaseInteracting.dataBaseToVectorSchemaRoot(list);
+      System.out.println(vectorSchemaRoot.getFieldVectors());
+    }
   }
 }
